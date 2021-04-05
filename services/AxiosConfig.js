95af163cf539e87ece9f2457e64136cca9313c8a
Enri <ibenrique2510@gmail.com>
2021-04-05 22:28:10 +0700

    Resolving PR comments

--- services/AxiosConfig.js
+++ services/AxiosConfig.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import DataTransformer from '~/utilities/DataTransformer'
+import humps from 'humps'
 
 const apiClient = axios.create({
   baseURL: 'http://localhost:3001/',
@@ -11,11 +11,11 @@ const apiClient = axios.create({
   transformResponse: [
     // convert keys of returned data from snake_case to camelCase
     ...axios.defaults.transformResponse,
-    (data) => DataTransformer.camelizeKeys(data),
+    (data) => humps.camelizeKeys(data),
   ],
   transformRequest: [
     // convert keys of request data from camelCase to snake_case
-    (data) => DataTransformer.decamelizeKeys(data),
+    (data) => humps.decamelizeKeys(data),
     ...axios.defaults.transformRequest,
   ],
 })
